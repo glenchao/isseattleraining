874b5c2cd2eb20ab3074c17a3aac106f5ea09e96
Glen Chao <glenchao@outlook.com>
2015-01-18 00:22:58 -0800

    Set auto-retrieve interval to 15min

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,20 @@
 function getData() {
-    var container = $("#container");
-    $.get("http://api.openweathermap.org/data/2.5/weather?id=5809844", function(data) {
-        var htmlStr = "<h1>No</h1>";
-        for(var r in data.rain) {
-            if(Number(data.rain[r]) > 0) {
-                htmlStr = "<h1>Of course...</h1>";
-                break;
+    _fetch();
+    setInterval(_fetch, 15*60*1000);
+
+    function _fetch() {
+        var container = $("#container");
+        $.get("http://api.openweathermap.org/data/2.5/weather?id=5809844", function(data) {
+            var htmlStr = "<h1>Nope! =)</h1>";
+            for(var r in data.rain) {
+                if(Number(data.rain[r]) > 0) {
+                    htmlStr = "<h1>Of course...</h1>";
+                    break;
+                }
             }
-        }
-        container.html(htmlStr);
-    }).fail(function() {
-        container.html("<h1>Yeah... something went wrong...</h1>");
-    })
+            container.html(htmlStr);
+        }).fail(function() {
+            container.html("<h1>Yeah... something went wrong...</h1>");
+        });
+    }
 }
