791a89933bec33f62dbb59636f7f8816bd22d97f
Glen Chao <glenchao@outlook.com>
2015-02-09 00:42:44 -0800

    Add Rainyday.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,11 +4,18 @@ function getData() {
 
     function _fetch() {
         var container = $("#container");
+        var cloud = new RainyDay({
+            image: document.getElementById("backgroundImage")
+        });
         $.get("http://api.openweathermap.org/data/2.5/weather?id=5809844", function(data) {
             var htmlStr = "<h1>Nope! =)</h1>";
             for(var r in data.rain) {
                 if(Number(data.rain[r]) > 0) {
                     htmlStr = "<h1>Of course...</h1>";
+                    cloud.rain([
+                        [0, 2, 100], 
+                        [3, 3, 1] 
+                    ], 200);      // every 500ms
                     break;
                 }
             }
